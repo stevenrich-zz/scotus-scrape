23f6a53695816e9f00d9c314bb587d5925c9dc74
stevenrich <steven.rich@washpost.com>
2016-02-13 20:27:00 -0500

    adding initial scraper

---
+++ scrape.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from pyvirtualdisplay import Display
+from BeautifulSoup import BeautifulSoup
+import time
+
+outfile = open("supreme.html", "w")
+
+print "Scraping Supreme Court site..."
+
+display = Display(visible=0, size=(800,600))
+display.start()
+
+url = "http://www.supremecourt.gov/opinions/slipopinions.aspx"
+
+driver = webdriver.Firefox()
+driver.get(url)
+time.sleep(30)
+page = driver.page_source
+soup = BeautifulSoup(page)
+print >> outfile, soup.prettify()
+
+driver.quit()
+display.stop()
